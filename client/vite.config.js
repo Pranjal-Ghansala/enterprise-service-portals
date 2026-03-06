5353ef07731c6f0f748dfc849c8b18f9f900ea6e
Pranjal <pranjalghansala83@gmail.com>
2026-03-06 11:54:35 +0530

    cssfixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // Use process.env for Vite config, fallback if missing
 const API_URL = process.env.VITE_API_URL || "http://localhost:5000";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+     tailwindcss(),
+  ],
   server: {
     proxy: {
       "/api": {
