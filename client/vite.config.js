bc9f1974b0bd89cc3ed02711d4e157901ab98f0a
Pranjal <pranjalghansala83@gmail.com>
2026-03-07 04:21:40 +0530

    BUGFIX

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// Use process.env for Vite config, fallback if missing
-const API_URL = process.env.VITE_API_URL || "http://localhost:5000";
+// Use import.meta.env for Vite config
+const API_URL = "http://localhost:5000";
 
 export default defineConfig({
   plugins: [react(),
--- server/server.js
+++ server/server.js
@@ -24,10 +24,10 @@ app.use("/api/users", userRoutes);
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
